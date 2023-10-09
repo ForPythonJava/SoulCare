b3887333f19e07ce70fbfcbe763f04d67bc9b940
ForPythonJava <forpythonjava@gmail.com>
2023-10-09 12:20:05 +0530

    cc

--- COMMON/commonFooter.php
+++ COMMON/commonFooter.php
@@ -17,169 +17,27 @@
                             <p class="footer-widget__about-text">Counselling psychologists work with clients to
                                 help them improve their mental health and emotional </p>
                         </div>
-
-                        <ul class="footer-widget__about-social-link">
-                            <li>
-                                <a href="#"><span class="fab fa-facebook-f"></span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span class="fab fa-twitter"></span></a>
-                            </li>
-                            <li>
-                                <a href="#"><span class="fab fa-pinterest-p"></span></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!--End Footer Widget Column-->
-
-                <!--Start Footer Widget Column-->
-                <div class="col-xl-3 col-lg-6 col-md-6 wow animated fadeInUp" data-wow-delay="0.3s">
-                    <div class="footer-widget__column footer-widget__contact clearfix">
-                        <div class="footer-widget__title-box">
-                            <h3 class="footer-widget__title">Contact</h3>
-                        </div>
-                        <ul class="footer-widget__contact-list list-unstyled clearfix">
-                            <li>
-                                <div class="icon">
-                                    <span class="icon-location"></span>
-                                </div>
-                                <div class="text">
-                                    <p>Run Drive Suite Suite 14545 <br>
-                                        Melbourne Australia</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <span class="icon-email"></span>
-                                </div>
-                                <div class="text">
-                                    <p><a href="mailto:helpus24@gmail.com">helpus24@gmail.com</a></p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <span class="icon-phone"></span>
-                                </div>
-                                <div class="text">
-                                    <p><a href="tel:0881122334455">+088 11 22 33 44 55</a></p>
-                                </div>
-                            </li>
-                        </ul>
-
-                        <div class="footer-widget__contact-text">
-                            <div class="text-box">
-                                <p><a href="#">Lets start talking</a></p>
-                            </div>
-
-                            <div class="icon-box">
-                                <span class="icon-marketing"></span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <!--End Footer Widget Column-->
-
-                <!--Start Footer Widget Column-->
-                <div class="col-xl-2 col-lg-6 col-md-6 wow animated fadeInUp" data-wow-delay="0.5s">
-                    <div class="footer-widget__column footer-widget__service clearfix">
-                        <div class="footer-widget__title-box">
-                            <h3 class="footer-widget__title">Essentials</h3>
-                        </div>
-                        <ul class="footer-widget__service-list list-unstyled clearfix">
-                            <li><a href="about.html">Privacy Policy</a></li>
-                            <li><a href="about.html">Terms of Use</a></li>
-                            <li><a href="about.html">Sitemap</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="team.html">Meet Our Team</a></li>
-                            <li><a href="blog-list.html">News & Media</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!--End Footer Widget Column-->
-
-                <!--Start Footer Widget Column-->
-                <div class="col-xl-4 col-lg-6 col-md-6 wow animated fadeInUp" data-wow-delay="0.7s">
-                    <div class="footer-widget__column footer-widget__newsletter clearfix">
-                        <div class="footer-widget__title-box">
-                            <h3 class="footer-widget__title">Newsletter</h3>
-                        </div>
-                        <p class="footer-widget__newsletter-text">Get the latest news other tips </p>
-                        <form class="footer-widget__subscribe-box">
-                            <div class="footer-widget__subscribe-input-box">
-                                <input type="email" placeholder="Email Address" name="email">
-                                <button type="submit" class="footer-widget__subscribe-btn"><i
-                                        class="icon-email3"></i></button>
-                            </div>
-                        </form>
-                        <p class="footer-widget__newsletter-text2">Looking to kick-off your project in the <br>
-                            next 30-90 days? Let’s start planning! </p>
                     </div>
                 </div>
                 <!--End Footer Widget Column-->
             </div>
         </div>
-    </div>
 
-    <div class="site-footer__bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
+        <div class="site-footer__bottom">
+            <div class="container">
+                <div class="row">
+                    <!-- <div class="col-xl-12">
                     <div class="site-footer__bottom-inner">
                         <p class="site-footer__bottom-text">Copyright©2023 <a href="index.html">SoulCare</a> All
                             Rights Reserved</p>
                     </div>
+                </div> -->
                 </div>
             </div>
         </div>
-    </div>
 </footer>
 <!--End Site Footer-->
 
-</div><!-- /.page-wrapper -->
-
-
-<div class="mobile-nav__wrapper">
-    <div class="mobile-nav__overlay mobile-nav__toggler"></div>
-    <!-- /.mobile-nav__overlay -->
-    <div class="mobile-nav__content">
-        <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
-
-        <div class="logo-box">
-            <a href="#">
-                <h2>SOUL<span id="h2">CARE</span></h2>
-            </a>
-        </div>
-        <!-- /.logo-box -->
-        <div class="mobile-nav__container"></div>
-        <!-- /.mobile-nav__container -->
-
-        <ul class="mobile-nav__contact list-unstyled">
-            <li>
-                <i class="fa fa-envelope"></i>
-                <a href="mailto:needhelp@packageName__.com">needhelp@uterpy.com</a>
-            </li>
-            <li>
-                <i class="fa fa-phone-alt"></i>
-                <a href="tel:666-888-0000">666 888 0000</a>
-            </li>
-        </ul><!-- /.mobile-nav__contact -->
-        <div class="mobile-nav__top">
-            <div class="mobile-nav__social">
-                <a href="#" class="fab fa-twitter"></a>
-                <a href="#" class="fab fa-facebook-square"></a>
-                <a href="#" class="fab fa-pinterest-p"></a>
-                <a href="#" class="fab fa-instagram"></a>
-            </div><!-- /.mobile-nav__social -->
-        </div><!-- /.mobile-nav__top -->
-
-
-
-    </div>
-    <!-- /.mobile-nav__content -->
-</div>
-<!-- /.mobile-nav__wrapper -->
 
 <div class="search-popup">
     <div class="search-popup__overlay search-toggler"></div>
